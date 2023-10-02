1c75f778e8d6383cdcd3c2a2a839e1df88ad3a26
CAMILO <CAMILO@LAPTOP-I12KO48S>
2023-10-02 12:55:22 -0500

    feat:clean start up

--- lib/main.dart
+++ lib/main.dart
@@ -9,34 +9,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xffD6D58E),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Bienvenido a camsapp',
-                            style:TextStyle(color:Color(0xff005C53),
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold)
-                            ),         
-                const Text("Registrate"),
-                GestureDetector(
-                  onTap: () {},
-                  child: const Text("Presioname"),
-                ),
-                TextFormField(),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("Ingresar"))
-                ],
+       
             ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 }
