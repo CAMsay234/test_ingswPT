bf1d0137778b5c0749700d7c3771e12c3d2f7cf3
CAMILO <CAMILO@LAPTOP-I12KO48S>
2023-10-02 13:39:18 -0500

    feat: login page titulo

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,8 +7,30 @@ class LoginPage extends StatelessWidget{
 
   @override 
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Text("Predictor de Tumores"),
+    return  Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body:  Column(
+        children: [
+          //Image.asset('assets/logo_tumor.png'),
+          const Text ("Predictor de tumores",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                             fontSize: 96,
+                             color:  Color(0xFF002333)
+                      ),
+                    ),
+          TextFormField(),
+          const Spacer(),
+          const SizedBox(
+            height: 60,
+            width: 120,
+            child: InkWell(),
+          ),
+          const Spacer(),
+        ],
+       
+      
+      )     
     );
   }
 } 
