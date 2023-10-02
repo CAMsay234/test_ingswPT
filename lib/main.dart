b83c2547284b40c5419584174fa0ca71ed2d208b
CAMILO <CAMILO@LAPTOP-I12KO48S>
2023-10-02 13:07:43 -0500

    feat: login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/login_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-       
-            ),
+      home: LoginPage(),
           );
   }
 }
