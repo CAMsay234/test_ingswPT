94dfbcc05cb1dde2e657dfb1bf76803e1293b511
CAMILO <CAMILO@LAPTOP-I12KO48S>
2023-10-02 22:43:29 -0500

    feat: primer diseño loggin

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,35 +2,85 @@
 
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  @override 
-  Widget build(BuildContext context){
-    return  Scaffold(
-      backgroundColor: const Color(0xFFFFFFFF),
-      body:  Column(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
         children: [
-          //Image.asset('assets/logo_tumor.png'),
-          const Text ("Predictor de tumores",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                             fontSize: 96,
-                             color:  Color(0xFF002333)
+          AppBar(
+            title: const Text(
+              "Online Instituto Médico UPB",
+              style: TextStyle(fontSize: 32, color: Color(0xFFB4BEC9)),
+            ),
+          ),
+          Container(
+            color: const Color(0xFFFFFFFF),
+            height: 2,
+          ),
+          Container(
+            color: const Color(0xff159A9C),
+            child: const Text(
+              "Predictor de Tumores",
+              style: TextStyle(fontSize: 96, color: Color(0xFF002333)),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Añadir espacio entre los elementos
+                children: [
+                  const Text(
+                    "Bienvenido",
+                    style: TextStyle(fontSize: 40, color: Color(0xFF002333)),
+                  ),
+                  const SizedBox(height: 32), // Aumentar el espacio entre los elementos
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "ID",
+                      labelStyle: TextStyle(color: Color(0xFF002333)),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF002333)),
                       ),
                     ),
-          TextFormField(),
-          const Spacer(),
-          const SizedBox(
-            height: 60,
-            width: 120,
-            child: InkWell(),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF6EEAB0),
+                        ),
+                        child: const Text(
+                          "Ingresar",
+                          style: TextStyle(color: Color(0xFF002333)),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF6EEAB0),
+                        ),
+                        child: const Text(
+                          "Registrarse",
+                          style: TextStyle(color: Color(0xFF002333)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
-          const Spacer(),
         ],
-       
-      
-      )     
+      ),
     );
   }
-} 
+}
