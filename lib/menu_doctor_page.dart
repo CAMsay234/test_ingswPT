e8c16179a5f7ab0c9c17a9c042657181d9a9283c
CAMILO <CAMILO@LAPTOP-I12KO48S>
2023-10-04 19:56:46 -0500

    feat: registro_page y menu_doctor_page appbar

--- lib/menu_doctor_page.dart
+++ lib/menu_doctor_page.dart
@@ -5,8 +5,34 @@ class MenuDoctorPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return  Scaffold(
+ backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+      body: Column(
+        children: [
+           AppBar(
+            backgroundColor: const Color(0xFFFFFFFF),
+            title: Row(
+              children: [
+                Image.asset(
+                  'assets/logo_corazon.png',
+                  width: 40,
+                  height: 40,
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  "Online Instituto Médico UPB",
+                  style: TextStyle(fontSize: 32, color: Color(0xFFB4BEC9)),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: const Color(0xFFFFFFFF),
+            height: 5,
+          ),
+          Container()
+        ],
+      )
     );
   }
 }
