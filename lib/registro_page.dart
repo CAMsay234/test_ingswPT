93fdd90ef138535749266c539a3e2139ab43badf
CAMILO <CAMILO@LAPTOP-I12KO48S>
2023-10-09 20:19:16 -0500

    fix: arreglo fecha nacimiento registro page

--- lib/registro_page.dart
+++ lib/registro_page.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
+
 import 'package:image_picker/image_picker.dart';
+import 'dart:core';
+import 'package:intl/intl.dart';
+
+
 
 class RegistroPage extends StatefulWidget {
   const RegistroPage({super.key});
@@ -10,14 +17,42 @@ class RegistroPage extends StatefulWidget {
 }
 
 class _RegistroPageState extends State<RegistroPage> {
-  String selectedSexo = 'Masculino'; // Valor inicial seleccionado
+  String? selectedSexo; // Valor inicial seleccionado
 
-  List<String> opcionesSexo = [
-    'Masculino',
-    'Femenino',
-    'Otro'
-  ]; // Opciones disponibles
-  XFile? _image; // Almacenar la imagen seleccionada por el usuario
+  List<String> opcionesSexo = ['Masculino', 'Femenino', 'Otro'];
+  String? selectedRh; // Variable para almacenar la opción seleccionada
+
+  List<String> opcionesRh = [
+    'O+',
+    'O-',
+    'A+',
+    'A-',
+    'B+',
+    'B-',
+    'AB+',
+    'AB-'
+  ]; // Opciones disponibles // Opciones disponibles
+  XFile? _image;
+  DateTime selectedDate = DateTime.now();
+  TextEditingController fechaNacimientoController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+
+    if (pickedDate != null && pickedDate != selectedDate) {
+      setState(() {
+        selectedDate = pickedDate;
+        fechaNacimientoController.text =
+            DateFormat('yyyy-MM-dd').format(selectedDate);
+      });
+    }
+  }
+  
 
   // ignore: unused_element
   Future<void> _getImage() async {
@@ -175,24 +210,13 @@ class _RegistroPageState extends State<RegistroPage> {
             ],
           ),
         ),
-        const SizedBox(
-            height:
-                100), // Espacio entre el contenido anterior y los datos a ingresar
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const Text("Fecha de Nacimiento:",
-                style: TextStyle(fontSize: 20, color: Colors.black)),
-            Container(
-                color: Colors.grey,
-                padding: const EdgeInsets.all(8),
-                child: const Text(
-                  "28/07/1999",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                )),
+    const SizedBox(
+              height: 100,
+            ), // Espacio entre el contenido anterior y los datos a ingresar
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                
                 SizedBox(
                   width: 300,
                   child: TextFormField(
@@ -205,36 +229,60 @@ class _RegistroPageState extends State<RegistroPage> {
                     ),
                   ),
                 ),
-              ],
-            ),
-            const Text("Rh:",
-                style: TextStyle(fontSize: 20, color: Colors.black)),
-            Container(
-                color: Colors.grey,
-                padding: const EdgeInsets.all(8),
-                child: const Text(
-                  "O-",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    controller: fechaNacimientoController,
+                    onTap: () => _selectDate(context),
+                    decoration: const InputDecoration(
+                      labelText: 'Fecha de Nacimiento',
+                    ),
+                  ),
+                ),
+                const Text(
+                  "Rh:",
+                  style: TextStyle(fontSize: 20,  color: Color(0xFF002333)),
+                ),
+                const SizedBox(
+                  width: 1,
+                ), // Espacio entre el título y la lista desplegable
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 6,
+                  ), // Ajusta el espacio vertical según tus preferencias
+                  child: DropdownButton<String>(
+                    value: selectedRh,
+                    hint: const Text("Selecciona el Rh"),
+                    onChanged: (String? newValue) {
+                      // Cuando se selecciona una opción, actualiza la variable selectedRh
+                      setState(() {
+                        selectedRh = newValue;
+                      });
+                    },
+                    items: opcionesRh.map((String opcion) {
+                      return DropdownMenuItem<String>(
+                        value: opcion,
+                        child: Text(opcion),
+                      );
+                    }).toList(),
+                  ),
+                ),
                 const Text(
                   "Sexo:",
-                  style: TextStyle(fontSize: 20, color: Colors.black),
+                  style: TextStyle(fontSize: 20, color: Color(0xFF002333)),
                 ),
                 Container(
                   margin: const EdgeInsets.only(
                       left: 10.0), // Ajusta el espacio aquí
                   child: DropdownButton<String?>(
                     value: selectedSexo,
+                    hint: const Text("Selecciona el sexo"),
                     onChanged: (String? newValue) {
                       setState(() {
-                        selectedSexo = newValue ?? 'Masculino';
+                        selectedSexo = newValue;
                       });
                     },
-                    items: opcionesSexo
-                        .map<DropdownMenuItem<String?>>((String value) {
+                    items: opcionesSexo.map<DropdownMenuItem<String?>>((String value) {
                       return DropdownMenuItem<String?>(
                         value: value,
                         child: Text(value),
@@ -244,92 +292,94 @@ class _RegistroPageState extends State<RegistroPage> {
                 ),
               ],
             ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "País",
+                      labelStyle: TextStyle(color: Color(0xFF002333)),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF002333)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Departamento",
+                      labelStyle: TextStyle(color: Color(0xFF002333)),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF002333)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Ciudad",
+                      labelStyle: TextStyle(color: Color(0xFF002333)),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF002333)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Dirección",
+                      labelStyle: TextStyle(color: Color(0xFF002333)),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF002333)),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 70),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción cuando se presiona "Guardar"
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        const Color(0xFF6EEAB0), // Fondo verde fosforescente
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 40), // Tamaño del botón cuadrado
+                    elevation: 10, // Valor de elevación
+                  ),
+                  child: const Text(
+                    "Guardar",
+                    textAlign: TextAlign
+                        .center, // Alinea el texto al centro dentro del botón
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color(0xFF002333), // Texto en azul oscuro
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
-
-        const SizedBox(height: 50),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "País",
-                  labelStyle: TextStyle(color: Color(0xFF002333)),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF002333)),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Departamento",
-                  labelStyle: TextStyle(color: Color(0xFF002333)),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF002333)),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Ciudad",
-                  labelStyle: TextStyle(color: Color(0xFF002333)),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF002333)),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 300,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Dirección",
-                  labelStyle: TextStyle(color: Color(0xFF002333)),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF002333)),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 70),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // Acción cuando se presiona "Registrar Paciente"
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    const Color(0xFF6EEAB0), // Fondo verde fosforescente
-                padding: const EdgeInsets.symmetric(
-                    vertical: 20, horizontal: 40), // Tamaño del botón cuadrado
-                elevation: 10, // Valor de elevación
-              ),
-              child: const Text(
-                "Guardar",
-                textAlign: TextAlign
-                    .center, // Alinea el texto al centro dentro del botón
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xFF002333), // Texto en azul oscuro
-                ),
-              ),
-            ),
-          ],
-        )
-      ] // Espacio entre el contenido anterior y los datos a ingresar
-          ),
-    );
+      );
+    
   }
+
 }
+
+
+
